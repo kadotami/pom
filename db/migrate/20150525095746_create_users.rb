04844bc0f5e4263e1e3a8149b8699b4e8f4b82ef
kadotami <mondamin.kqfb@gmail.com>
2015-05-25 19:27:27 +0900

    add user name

--- db/migrate/20150525095746_create_users.rb
+++ db/migrate/20150525095746_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name, unique: true
       t.float :height
       t.float :weight
       t.float :fat_rate
